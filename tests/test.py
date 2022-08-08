06ddb5711df97c2b3188e808117c4d379a41e7ce
Fergal Walsh <fergal@hipolabs.com>
2022-08-08 12:29:54 +0300

    Ensure asset totals are >= ASSET_MIN_TOTAL

--- tests/test.py
+++ tests/test.py
@@ -40,6 +40,8 @@ APPLICATION_ID = 1
 APPLICATION_ADDRESS = get_application_address(APPLICATION_ID)
 print('App Address:', APPLICATION_ADDRESS)
 
+PROXY_APP_ID = 10
+
 
 def get_pool_logicsig_bytecode(asset_1_id, asset_2_id):
     # These are the bytes of the logicsig template. This needs to be updated if the logicsig is updated.
@@ -621,6 +623,56 @@ class TestBootstrap(BaseTestCase):
         with self.assertRaises(LogicEvalError) as e:
             self.ledger.eval_transactions(transactions)
         self.assertEqual(e.exception.source['line'], f'assert(Txn.ApplicationArgs[0] == "{METHOD_BOOTSTRAP}")')
+
+    def test_fail_bad_asset_1_total(self):
+        self.ledger.create_asset(self.asset_1_id, params=dict(unit_name="NFT", total=100))
+        self.ledger.create_asset(self.asset_2_id, params=dict(unit_name="BTC"))
+        lsig = get_pool_logicsig_bytecode(self.asset_1_id, self.asset_2_id)
+        pool_address = lsig.address()
+        self.ledger.set_account_balance(pool_address, 2_000_000)
+        transactions = [
+            transaction.LogicSigTransaction(
+                transaction.ApplicationOptInTxn(
+                    sender=lsig.address(),
+                    sp=self.sp,
+                    index=APPLICATION_ID,
+                    app_args=[METHOD_BOOTSTRAP, self.asset_1_id, self.asset_2_id],
+                    foreign_assets=[self.asset_1_id, self.asset_2_id],
+                    rekey_to=APPLICATION_ADDRESS,
+                ),
+                lsig
+            )
+        ]
+        transactions[0].transaction.fee = self.minimum_fee - 1
+
+        with self.assertRaises(LogicEvalError) as e:
+            self.ledger.eval_transactions(transactions)
+        self.assertEqual(e.exception.source['line'], 'assert(exists && (asset_total > ASSET_MIN_TOTAL_SUPPLY))')
+
+    def test_fail_bad_asset_2_total(self):
+        self.ledger.create_asset(self.asset_1_id, params=dict(unit_name="USDC"))
+        self.ledger.create_asset(self.asset_2_id, params=dict(unit_name="NFT", total=1))
+        lsig = get_pool_logicsig_bytecode(self.asset_1_id, self.asset_2_id)
+        pool_address = lsig.address()
+        self.ledger.set_account_balance(pool_address, 2_000_000)
+        transactions = [
+            transaction.LogicSigTransaction(
+                transaction.ApplicationOptInTxn(
+                    sender=lsig.address(),
+                    sp=self.sp,
+                    index=APPLICATION_ID,
+                    app_args=[METHOD_BOOTSTRAP, self.asset_1_id, self.asset_2_id],
+                    foreign_assets=[self.asset_1_id, self.asset_2_id],
+                    rekey_to=APPLICATION_ADDRESS,
+                ),
+                lsig
+            )
+        ]
+        transactions[0].transaction.fee = self.minimum_fee - 1
+
+        with self.assertRaises(LogicEvalError) as e:
+            self.ledger.eval_transactions(transactions)
+        self.assertEqual(e.exception.source['line'], 'assert(exists && (asset_total > ASSET_MIN_TOTAL_SUPPLY))')
 
 
 class TestBootstrapAlgoPair(BaseTestCase):
@@ -3129,6 +3181,261 @@ class TestSetFee(BaseTestCase):
                 }
             }
         )
+
+proxy_approval_program = TealishProgram(tealish="""
+    #pragma version 7
+
+    const int TINYMAN_APP_ID = 1
+    const int FEE_BASIS_POINTS = 100
+
+    assert(Gtxn[0].AssetReceiver == Global.CurrentApplicationAddress)
+    int swap_amount = (Gtxn[0].AssetAmount * (10000 - FEE_BASIS_POINTS)) / 10000
+    int initial_output_balance
+    _, initial_output_balance = asset_holding_get(AssetBalance, Global.CurrentApplicationAddress, Txn.Assets[1])
+    inner_group:
+        inner_txn:
+            TypeEnum: Axfer
+            Fee: 0
+            AssetReceiver: Txn.Accounts[1]
+            XferAsset: Gtxn[0].XferAsset
+            AssetAmount: swap_amount
+        end
+        inner_txn:
+            TypeEnum: Appl
+            Fee: 0
+            ApplicationID: TINYMAN_APP_ID
+            ApplicationArgs[0]: "swap"
+            ApplicationArgs[1]: Txn.ApplicationArgs[1]
+            ApplicationArgs[2]: Txn.ApplicationArgs[2]
+            ApplicationArgs[3]: Txn.ApplicationArgs[3]
+            ApplicationArgs[4]: "fixed-input"
+            Accounts[0]: Txn.Accounts[1]
+            Assets[0]: Txn.Assets[0]
+            Assets[1]: Txn.Assets[1]
+        end
+    end
+
+    int new_output_balance
+    _, new_output_balance = asset_holding_get(AssetBalance, Global.CurrentApplicationAddress, Txn.Assets[1])
+    int output_amount = new_output_balance - initial_output_balance
+    inner_txn:
+        TypeEnum: Axfer
+        Fee: 0
+        AssetReceiver: Txn.Sender
+        XferAsset: Txn.Assets[1]
+        AssetAmount: output_amount
+    end
+    exit(1)
+""")
+
+PROXY_ADDRESS = get_application_address(PROXY_APP_ID)
+
+
+class TestProxySwap(BaseTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sp = get_suggested_params()
+        cls.asset_1_id = 5
+        cls.asset_2_id = 2
+
+    def setUp(self):
+        self.ledger = JigLedger()
+        self.create_amm_app()
+        self.ledger.set_account_balance(user_addr, 1_000_000)
+        self.ledger.set_account_balance(user_addr, 1_000_000, asset_id=self.asset_1_id)
+        self.ledger.set_account_balance(user_addr, 0, asset_id=self.asset_2_id)
+
+        self.ledger.create_app(app_id=PROXY_APP_ID, approval_program=proxy_approval_program, creator=app_creator_address)
+        self.ledger.set_account_balance(PROXY_ADDRESS, 1_000_000)
+        self.ledger.set_account_balance(PROXY_ADDRESS, 0, asset_id=self.asset_1_id)
+        self.ledger.set_account_balance(PROXY_ADDRESS, 0, asset_id=self.asset_2_id)
+
+        lsig = get_pool_logicsig_bytecode(self.asset_1_id, self.asset_2_id)
+        self.pool_address = lsig.address()
+        self.ledger.set_account_balance(self.pool_address, 1_000_000)
+        self.ledger.set_auth_addr(self.pool_address, APPLICATION_ADDRESS)
+
+    def test_pass(self):
+        self.ledger.set_account_balance(self.pool_address, 1_000_000, asset_id=self.asset_1_id)
+        self.ledger.set_account_balance(self.pool_address, 1_000_000, asset_id=self.asset_2_id)
+        self.ledger.set_local_state(
+            address=self.pool_address,
+            app_id=APPLICATION_ID,
+            state={
+                b'asset_1_id': self.asset_1_id,
+                b'asset_2_id': self.asset_2_id,
+                b'asset_1_reserves': 1_000_000,
+                b'asset_2_reserves': 1_000_000,
+                b'poolers_fee_share': POOLERS_FEE_SHARE,
+                b'protocol_fee_share': PROTOCOL_FEE_SHARE,
+            }
+        )
+
+        txn_group = [
+            transaction.AssetTransferTxn(
+                sender=user_addr,
+                sp=self.sp,
+                receiver=PROXY_ADDRESS,
+                index=self.asset_1_id,
+                amt=10_000,
+            ),
+            transaction.ApplicationNoOpTxn(
+                sender=user_addr,
+                sp=self.sp,
+                index=PROXY_APP_ID,
+                app_args=[METHOD_SWAP, self.asset_1_id, self.asset_2_id, 9000],
+                foreign_assets=[self.asset_1_id, self.asset_2_id],
+                foreign_apps=[APPLICATION_ID],
+                accounts=[self.pool_address],
+            )
+        ]
+        txn_group[1].fee = 5000
+
+        txn_group = transaction.assign_group_id(txn_group)
+        stxns = [
+            txn_group[0].sign(user_sk),
+            txn_group[1].sign(user_sk),
+        ]
+
+        block = self.ledger.eval_transactions(stxns)
+        txns = block[b'txns']
+        itxn = txns[1][b'dt'][b'itx'][-1][b'txn']
+        self.assertEqual(itxn[b'aamt'], 9776)
+        self.assertEqual(itxn[b'arcv'], decode_address(user_addr))
+        self.assertEqual(itxn[b'xaid'], self.asset_2_id)
+        self.assertEqual(itxn[b'snd'], decode_address(PROXY_ADDRESS))
+
+        self.assertEqual(self.ledger.get_account_balance(PROXY_ADDRESS, self.asset_1_id)[0], 100)
+
+        # do the same swap again and watch the fees accumulate
+        block = self.ledger.eval_transactions(stxns)
+        self.assertEqual(self.ledger.get_account_balance(PROXY_ADDRESS, self.asset_1_id)[0], 200)
+
+
+
+class TestGroupedSwap(BaseTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sp = get_suggested_params()
+        cls.asset_1_id = 5
+        cls.asset_2_id = 2
+        cls.asset_3_id = 7
+
+    def setUp(self):
+        self.ledger = JigLedger()
+        self.create_amm_app()
+        self.ledger.set_account_balance(user_addr, 1_000_000)
+        self.ledger.set_account_balance(user_addr, 1_000_000, asset_id=self.asset_1_id)
+        self.ledger.set_account_balance(user_addr, 0, asset_id=self.asset_2_id)
+        self.ledger.set_account_balance(user_addr, 0, asset_id=self.asset_3_id)
+
+        lsig1 = get_pool_logicsig_bytecode(self.asset_1_id, self.asset_2_id)
+        self.pool_address1 = lsig1.address()
+        self.ledger.set_account_balance(self.pool_address1, 1_000_000)
+        self.ledger.set_auth_addr(self.pool_address1, APPLICATION_ADDRESS)
+
+        lsig2 = get_pool_logicsig_bytecode(self.asset_2_id, self.asset_3_id)
+        self.pool_address2 = lsig2.address()
+        self.ledger.set_account_balance(self.pool_address2, 1_000_000)
+        self.ledger.set_auth_addr(self.pool_address2, APPLICATION_ADDRESS)
+
+    def test_pass(self):
+        self.ledger.set_account_balance(self.pool_address1, 1_000_000, asset_id=self.asset_1_id)
+        self.ledger.set_account_balance(self.pool_address1, 1_000_000, asset_id=self.asset_2_id)
+        self.ledger.set_local_state(
+            address=self.pool_address1,
+            app_id=APPLICATION_ID,
+            state={
+                b'asset_1_id': self.asset_1_id,
+                b'asset_2_id': self.asset_2_id,
+                b'asset_1_reserves': 1_000_000,
+                b'asset_2_reserves': 1_000_000,
+                b'poolers_fee_share': POOLERS_FEE_SHARE,
+                b'protocol_fee_share': PROTOCOL_FEE_SHARE,
+            }
+        )
+
+        self.ledger.set_account_balance(self.pool_address2, 1_000_000, asset_id=self.asset_2_id)
+        self.ledger.set_account_balance(self.pool_address2, 1_000_000, asset_id=self.asset_3_id)
+        self.ledger.set_local_state(
+            address=self.pool_address2,
+            app_id=APPLICATION_ID,
+            state={
+                b'asset_1_id': self.asset_2_id,
+                b'asset_2_id': self.asset_3_id,
+                b'asset_1_reserves': 1_000_000,
+                b'asset_2_reserves': 1_000_000,
+                b'poolers_fee_share': POOLERS_FEE_SHARE,
+                b'protocol_fee_share': PROTOCOL_FEE_SHARE,
+            }
+        )
+
+        txn_group = [
+            # Swap 1
+            transaction.AssetTransferTxn(
+                sender=user_addr,
+                sp=self.sp,
+                receiver=self.pool_address1,
+                index=self.asset_1_id,
+                amt=10_000,
+            ),
+            transaction.ApplicationNoOpTxn(
+                sender=user_addr,
+                sp=self.sp,
+                index=APPLICATION_ID,
+                app_args=[METHOD_SWAP, self.asset_1_id, self.asset_2_id, 9872, "fixed-input"],
+                foreign_assets=[self.asset_1_id, self.asset_2_id],
+                accounts=[self.pool_address1],
+            ),
+
+            # Swap 2
+            transaction.AssetTransferTxn(
+                sender=user_addr,
+                sp=self.sp,
+                receiver=self.pool_address2,
+                index=self.asset_2_id,
+                amt=9872,
+            ),
+            transaction.ApplicationNoOpTxn(
+                sender=user_addr,
+                sp=self.sp,
+                index=APPLICATION_ID,
+                app_args=[METHOD_SWAP, self.asset_2_id, self.asset_3_id, 9749, "fixed-input"],
+                foreign_assets=[self.asset_2_id, self.asset_3_id],
+                accounts=[self.pool_address2],
+            )
+        ]
+        txn_group[1].fee = 5000
+        txn_group[3].fee = 5000
+
+        txn_group = transaction.assign_group_id(txn_group)
+        stxns = [
+            txn_group[0].sign(user_sk),
+            txn_group[1].sign(user_sk),
+            txn_group[2].sign(user_sk),
+            txn_group[3].sign(user_sk),
+        ]
+
+        block = self.ledger.eval_transactions(stxns)
+        txns = block[b'txns']
+
+        itxn = txns[1][b'dt'][b'itx'][0][b'txn']
+        self.assertEqual(itxn[b'aamt'], 9872)
+        self.assertEqual(itxn[b'arcv'], decode_address(user_addr))
+        self.assertEqual(itxn[b'xaid'], self.asset_2_id)
+        self.assertEqual(itxn[b'snd'], decode_address(self.pool_address1))
+
+        itxn = txns[3][b'dt'][b'itx'][0][b'txn']
+        self.assertEqual(itxn[b'aamt'], 9749)
+        self.assertEqual(itxn[b'arcv'], decode_address(user_addr))
+        self.assertEqual(itxn[b'xaid'], self.asset_3_id)
+        self.assertEqual(itxn[b'snd'], decode_address(self.pool_address2))
+
+        self.assertEqual(self.ledger.get_account_balance(user_addr, self.asset_2_id)[0], 0)
+        self.assertEqual(self.ledger.get_account_balance(user_addr, self.asset_3_id)[0], 9749)
+
 
 
 if __name__ == '__main__':
