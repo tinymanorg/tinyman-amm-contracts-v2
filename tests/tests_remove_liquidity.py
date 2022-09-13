6504f81a0e063da1539e7ee100b942ac81b5113a
Fergal Walsh <fergal@hipolabs.com>
2022-09-13 15:04:21 +0100

    Refactor remove_liquidity to support single or double asset removal

--- tests/tests_remove_liquidity.py
+++ tests/tests_remove_liquidity.py
@@ -207,3 +207,279 @@ class TestRemoveLiquidity(BaseTestCase):
                     # local state delta
                     pool_local_state_delta = txn[b'dt'][b'ld'][1]
                     self.assertDictEqual(pool_local_state_delta, outputs["local_state_delta"])
+
+    def test_remove_liquidity_asset_1(self):
+        test_cases = [
+            dict(
+                msg="Test basic remove liquidity.",
+                initials=dict(
+                    asset_1_reserves=1_000_000,
+                    asset_2_reserves=1_000_000,
+                    issued_pool_token_amount=1_000_000,
+                ),
+                inputs=dict(
+                    removed_pool_token_amount=5_000,
+                ),
+                outputs=dict(
+                    asset_1_out=9960,
+                    local_state_delta={
+                        b'asset_1_reserves': {b'at': 2, b'ui': 1_000_000 - 9960},
+                        b'asset_2_protocol_fees': {b'at': 2, b'ui': 2},
+                        b'asset_2_reserves': {b'at': 2, b'ui': 1_000_000 - 2},
+                        b'issued_pool_tokens': {b'at': 2, b'ui': 1_000_000 - 5_000},
+                    }
+                )
+            ),
+            dict(
+                msg="Test removing 0 pool token. It should fail because asset out amounts are 0.",
+                initials=dict(
+                    asset_1_reserves=1_000_000,
+                    asset_2_reserves=1_000_000,
+                    issued_pool_token_amount=1_000_000,
+                ),
+                inputs=dict(
+                    removed_pool_token_amount=0,
+                ),
+                exception=dict(
+                    source_line='assert(asset_1_amount && asset_2_amount)'
+                )
+            ),
+            dict(
+                msg="Test removing all pool tokens. It should fail because the swap will be impossible.",
+                initials=dict(
+                    asset_1_reserves=1_000_000,
+                    asset_2_reserves=1_000_000,
+                    issued_pool_token_amount=1_000_000,
+                ),
+                inputs=dict(
+                    removed_pool_token_amount=999_000,
+                ),
+                exception=dict(
+                    source_line='assert(asset_2_amount < asset_2_reserves)'
+                )
+            ),
+        ]
+
+        for test_case in test_cases:
+            with self.subTest(**test_case):
+                initials = test_case["initials"]
+                inputs = test_case["inputs"]
+
+                self.reset_ledger()
+                self.set_initial_pool_liquidity(asset_1_reserves=initials["asset_1_reserves"], asset_2_reserves=initials["asset_2_reserves"], liquidity_provider_address=self.user_addr)
+                self.assertEqual(initials["issued_pool_token_amount"], self.ledger.accounts[self.pool_address]['local_states'][APPLICATION_ID][b'issued_pool_tokens'])
+
+                txn_group = self.get_remove_liquidity_transactions(liquidity_asset_amount=inputs["removed_pool_token_amount"], app_call_fee=2_000)
+                txn_group[1].foreign_assets = [self.asset_1_id]
+                txn_group = transaction.assign_group_id(txn_group)
+                stxns = self.sign_txns(txn_group, self.user_sk)
+
+                if exception := test_case.get("exception"):
+                    with self.assertRaises(LogicEvalError) as e:
+                        self.ledger.eval_transactions(stxns)
+
+                    self.assertEqual(e.exception.source['line'], exception.get("source_line"))
+
+                else:
+                    outputs = test_case["outputs"]
+
+                    block = self.ledger.eval_transactions(stxns)
+                    block_txns = block[b'txns']
+
+                    # outer transactions
+                    self.assertEqual(len(block_txns), 2)
+
+                    # outer transactions [0]
+                    txn = block_txns[0]
+                    self.assertEqual(
+                        txn[b'txn'],
+                        {
+                            b'aamt': inputs["removed_pool_token_amount"],
+                            b'arcv': decode_address(self.pool_address),
+                            b'fee': self.sp.fee,
+                            b'fv': self.sp.first,
+                            b'grp': ANY,
+                            b'lv': self.sp.last,
+                            b'snd': decode_address(self.user_addr),
+                            b'type': b'axfer',
+                            b'xaid': self.pool_token_asset_id
+                        }
+                    )
+
+                    # outer transactions [1]
+                    txn = block_txns[1]
+                    self.assertEqual(
+                        txn[b'txn'],
+                        {
+                            b'apaa': [b'remove_liquidity'],
+                            b'apas': [self.asset_1_id],
+                            b'apat': [decode_address(self.pool_address)],
+                            b'apid': APPLICATION_ID,
+                            b'fee': self.sp.fee * 2,
+                            b'fv': self.sp.first,
+                            b'grp': ANY,
+                            b'lv': self.sp.last,
+                            b'snd': decode_address(self.user_addr),
+                            b'type': b'appl'
+                        }
+                    )
+
+                    # inner transactions
+                    inner_transactions = txn[b'dt'][b'itx']
+                    self.assertEqual(len(inner_transactions), 1)
+
+                    # inner transactions - [0]
+                    self.assertDictEqual(
+                        inner_transactions[0][b'txn'],
+                        {
+                            b'aamt': outputs["asset_1_out"],
+                            b'arcv': decode_address(self.user_addr),
+                            b'fv': self.sp.first,
+                            b'lv': self.sp.last,
+                            b'snd': decode_address(self.pool_address),
+                            b'type': b'axfer',
+                            b'xaid': self.asset_1_id
+                        }
+                    )
+
+                    # local state delta
+                    pool_local_state_delta = txn[b'dt'][b'ld'][1]
+                    self.assertDictEqual(pool_local_state_delta, outputs["local_state_delta"])
+
+    def test_remove_liquidity_asset_2(self):
+        test_cases = [
+            dict(
+                msg="Test basic remove liquidity.",
+                initials=dict(
+                    asset_1_reserves=1_000_000,
+                    asset_2_reserves=1_000_000,
+                    issued_pool_token_amount=1_000_000,
+                ),
+                inputs=dict(
+                    removed_pool_token_amount=5_000,
+                ),
+                outputs=dict(
+                    asset_2_out=9960,
+                    local_state_delta={
+                        b'asset_1_protocol_fees': {b'at': 2, b'ui': 2},
+                        b'asset_1_reserves': {b'at': 2, b'ui': 1_000_000 - 2},
+                        b'asset_2_reserves': {b'at': 2, b'ui': 1_000_000 - 9960},
+                        b'issued_pool_tokens': {b'at': 2, b'ui': 1_000_000 - 5_000},
+                    }
+                )
+            ),
+            dict(
+                msg="Test removing 0 pool token. It should fail because asset out amounts are 0.",
+                initials=dict(
+                    asset_1_reserves=1_000_000,
+                    asset_2_reserves=1_000_000,
+                    issued_pool_token_amount=1_000_000,
+                ),
+                inputs=dict(
+                    removed_pool_token_amount=0,
+                ),
+                exception=dict(
+                    source_line='assert(asset_1_amount && asset_2_amount)'
+                )
+            ),
+            dict(
+                msg="Test removing all pool tokens. It should fail because the swap will be impossible.",
+                initials=dict(
+                    asset_1_reserves=1_000_000,
+                    asset_2_reserves=1_000_000,
+                    issued_pool_token_amount=1_000_000,
+                ),
+                inputs=dict(
+                    removed_pool_token_amount=999_000,
+                ),
+                exception=dict(
+                    source_line='assert(asset_1_amount < asset_1_reserves)'
+                )
+            ),
+        ]
+
+        for test_case in test_cases:
+            with self.subTest(**test_case):
+                initials = test_case["initials"]
+                inputs = test_case["inputs"]
+
+                self.reset_ledger()
+                self.set_initial_pool_liquidity(asset_1_reserves=initials["asset_1_reserves"], asset_2_reserves=initials["asset_2_reserves"], liquidity_provider_address=self.user_addr)
+                self.assertEqual(initials["issued_pool_token_amount"], self.ledger.accounts[self.pool_address]['local_states'][APPLICATION_ID][b'issued_pool_tokens'])
+
+                txn_group = self.get_remove_liquidity_transactions(liquidity_asset_amount=inputs["removed_pool_token_amount"], app_call_fee=2_000)
+                txn_group[1].foreign_assets = [self.asset_2_id]
+                txn_group = transaction.assign_group_id(txn_group)
+                stxns = self.sign_txns(txn_group, self.user_sk)
+
+                if exception := test_case.get("exception"):
+                    with self.assertRaises(LogicEvalError) as e:
+                        self.ledger.eval_transactions(stxns)
+
+                    self.assertEqual(e.exception.source['line'], exception.get("source_line"))
+
+                else:
+                    outputs = test_case["outputs"]
+
+                    block = self.ledger.eval_transactions(stxns)
+                    block_txns = block[b'txns']
+
+                    # outer transactions
+                    self.assertEqual(len(block_txns), 2)
+
+                    # outer transactions [0]
+                    txn = block_txns[0]
+                    self.assertEqual(
+                        txn[b'txn'],
+                        {
+                            b'aamt': inputs["removed_pool_token_amount"],
+                            b'arcv': decode_address(self.pool_address),
+                            b'fee': self.sp.fee,
+                            b'fv': self.sp.first,
+                            b'grp': ANY,
+                            b'lv': self.sp.last,
+                            b'snd': decode_address(self.user_addr),
+                            b'type': b'axfer',
+                            b'xaid': self.pool_token_asset_id
+                        }
+                    )
+
+                    # outer transactions [1]
+                    txn = block_txns[1]
+                    self.assertEqual(
+                        txn[b'txn'],
+                        {
+                            b'apaa': [b'remove_liquidity'],
+                            b'apas': [self.asset_2_id],
+                            b'apat': [decode_address(self.pool_address)],
+                            b'apid': APPLICATION_ID,
+                            b'fee': self.sp.fee * 2,
+                            b'fv': self.sp.first,
+                            b'grp': ANY,
+                            b'lv': self.sp.last,
+                            b'snd': decode_address(self.user_addr),
+                            b'type': b'appl'
+                        }
+                    )
+
+                    # inner transactions
+                    inner_transactions = txn[b'dt'][b'itx']
+                    self.assertEqual(len(inner_transactions), 1)
+
+                    # inner transactions - [0]
+                    self.assertDictEqual(
+                        inner_transactions[0][b'txn'],
+                        {
+                            b'aamt': outputs["asset_2_out"],
+                            b'arcv': decode_address(self.user_addr),
+                            b'fv': self.sp.first,
+                            b'lv': self.sp.last,
+                            b'snd': decode_address(self.pool_address),
+                            b'type': b'axfer',
+                            b'xaid': self.asset_2_id
+                        }
+                    )
+
+                    # local state delta
+                    pool_local_state_delta = txn[b'dt'][b'ld'][1]
+                    self.assertDictEqual(pool_local_state_delta, outputs["local_state_delta"])
